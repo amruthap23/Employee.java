298e32219f74f6fabc4ad468c05c2f0558ce1cd8
amruthap23 <85816788+amruthap23@users.noreply.github.com>
2021-08-28 08:12:37 +0530

    Using Java displaying Employee details

---
+++ Employee.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Employee 
+{
+	public static void main(String[] args) {
+		System.out.println("Name  \t Year of joining\t Address");
+		System.out.println("Robert\t 1994           \t 64C-WallsStreet");
+		System.out.println("Sam   \t 2000           \t 68D-WallsStreet");
+		System.out.println("Jhon  \t 1999           \t 64C-WallsStreet");
+	}
+}
